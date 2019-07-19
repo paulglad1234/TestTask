4f594bb7115ede48bdc7145714d67daa6fcc2dde
paulglad1234 <40827217+paulglad1234@users.noreply.github.com>
2019-07-19 15:01:05 +0300

    LessUndone
    Научился принимать данные в формате json.

--- MyFirstWebApplication/FileWorkerMiddleware.cs
+++ MyFirstWebApplication/FileWorkerMiddleware.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using MyFirstWebApplication.Services;
+using Microsoft.AspNetCore.Http.Internal;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace MyFirstWebApplication
 {
@@ -18,26 +21,52 @@ namespace MyFirstWebApplication
         public async Task InvokeAsync(HttpContext context)
         {
             const string filename = "values.json";
-            if (context.Request.Query.Count != 0)
+
+            var request = ReadBodyAsString(context.Request);
+
+            if (request != string.Empty)
             {
+                var jsonRequest = JObject.Parse(request);
                 if (context.Request.Method == "POST")
-                    await PostHandler(context, filename);
+                    await PostHandler(jsonRequest, filename);
                 else
-                    await GetHandler(context, filename);
+                    await GetHandler(context, jsonRequest, filename);
             }
             else await _next.Invoke(context);
         }
 
-        private async Task PostHandler(HttpContext context, string filename)
+        private async Task PostHandler(JObject request, string filename)
         {
-            await Task.Run(() => { _fileWorker.AddToEnd(filename, int.Parse(context.Request.Query["value"])); });
+            await Task.Run(() => { _fileWorker.AddToEnd(filename, (int)request["value"]); });
         }
 
-        private async Task GetHandler(HttpContext context, string filename)
+        private async Task GetHandler(HttpContext context, JObject request, string filename)
         {
             await context.Response.WriteAsync(Newtonsoft.Json.JsonConvert.SerializeObject(new {
-                sum = _fileWorker.GetSum(filename, int.Parse(context.Request.Query["from"]),
-                    int.Parse(context.Request.Query["till"]))}));
+                sum = _fileWorker.GetSum(filename, (int)request["from"],
+                    (int)request["till"])}));
+        }
+        private static string ReadBodyAsString(HttpRequest request)
+        {
+            var initialBody = request.Body; // Workaround
+
+            try
+            {
+                request.EnableRewind();
+
+                using (var reader = new StreamReader(request.Body))
+                {
+                    var text = reader.ReadToEnd();
+                    return text;
+                }
+            }
+            finally
+            {
+                // Workaround so MVC action will be able to read body as well
+                request.Body = initialBody;
+            }
+
+            return string.Empty;
         }
     }
 }
